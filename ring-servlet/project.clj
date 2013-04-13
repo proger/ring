8713e8525ac4deb86d691a018e17a7bd9c7e6dfd
Vladimir Kirillov <proger@wilab.org.ua>
2013-04-13 17:57:12 +0300

    ring-servlet: bump version to -SNAPSHOT

--- ring-servlet/project.clj
+++ ring-servlet/project.clj
@@ -1,4 +1,4 @@
-(defproject ring/ring-servlet "1.2.0-beta2"
+(defproject ring/ring-servlet "1.2.0-beta2-SNAPSHOT"
   :description "Ring servlet utilities."
   :url "https://github.com/ring-clojure/ring"
   :license {:name "The MIT License"
